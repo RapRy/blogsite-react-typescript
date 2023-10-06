089d119318f4936cba7905bab894cf06ac4fdf68
ryan <rapry.olarte@gmail.com>
2023-10-06 12:06:43 +0800

    ad

--- src/components/common/menu/CategoriesMenu.tsx
+++ src/components/common/menu/CategoriesMenu.tsx
@@ -15,6 +15,8 @@ import { replaceAllSpaceWithHyphen } from "../../../utils/helpers/stringManipula
 const CategoriesMenu = () => {
   const { data: categoryList, isLoading, error } = useGetCategoriesQuery();
 
+  console.log("test");
+
   return (
     <>
       <Typography
